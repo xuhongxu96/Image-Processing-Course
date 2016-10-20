f573b3aa489f79fd2edc1526b8764be151a76e18
Hongxu Xu <xuhongxu96@gmail.com>
2016-10-20 18:47:04 +0800

    improved

--- BMPReader/xbitmap.cpp
+++ BMPReader/xbitmap.cpp
@@ -13,49 +13,52 @@ XBitmap::XBitmap(const char *filename)
 
 bool XBitmap::open(const char *filename)
 {
+    if (isOpen) {
+        delete [] buffer;
+    }
     isOpen = false;
 
-    std::FILE *fp = std::fopen(filename, "r");
+    FILE *fp = fopen(filename, "r");
     if (!fp) {
-        std::perror("Failed to open BMP file");
+        perror("Failed to open BMP file");
         return false;
     }
 
-    std::printf("Opened: %s\n", filename);
+    printf("Opened: %s\n", filename);
 
-    std::printf("\n-- File Header\n");
+    printf("\n-- File Header\n");
 
-    std::fread(&fileHeader, sizeof(XBMPFileHeader), 1, fp);
+    fread(&fileHeader, sizeof(XBMPFileHeader), 1, fp);
 
-    std::printf("file_type:\t%c%c\n", fileHeader.bm[0], fileHeader.bm[1]);
-    std::printf("file_size:\t%d\n", fileHeader.fileSize);
-    std::printf("data_offset:\t%d\n", fileHeader.offset);
-    std::printf("reserved:\t%d\n", fileHeader.reserved);
+    printf("file_type:\t%c%c\n", fileHeader.bm[0], fileHeader.bm[1]);
+    printf("file_size:\t%d\n", fileHeader.fileSize);
+    printf("data_offset:\t%d\n", fileHeader.offset);
+    printf("reserved:\t%d\n", fileHeader.reserved);
 
 
-    std::printf("\n-- DIB Header\n");
+    printf("\n-- DIB Header\n");
 
-    std::fread(&DIBHeader, sizeof(XBMPDIBHeader), 1, fp);
+    fread(&DIBHeader, sizeof(XBMPDIBHeader), 1, fp);
 
-    std::printf("header_size:\t%d\n", DIBHeader.size);
-    std::printf("image_size:\t%d * %d\n", DIBHeader.width, DIBHeader.height);
-    std::printf("plain_id:\t%d\n", DIBHeader.plain);
-    std::printf("bits_count:\t%d\n", DIBHeader.bits);
-    std::printf("compression:\t%d\n", DIBHeader.compression);
-    std::printf("data_size:\t%d\n", DIBHeader.dataSize);
-    std::printf("DPI:\t\t%d, %d\n", DIBHeader.dpiX, DIBHeader.dpiY);
-    std::printf("palette:\t%d\n", DIBHeader.palette);
-    std::printf("primary_color:\t%d\n", DIBHeader.primaryColorCount);
+    printf("header_size:\t%d\n", DIBHeader.size);
+    printf("image_size:\t%d * %d\n", DIBHeader.width, DIBHeader.height);
+    printf("plain_id:\t%d\n", DIBHeader.plain);
+    printf("bits_count:\t%d\n", DIBHeader.bits);
+    printf("compression:\t%d\n", DIBHeader.compression);
+    printf("data_size:\t%d\n", DIBHeader.dataSize);
+    printf("DPI:\t\t%d, %d\n", DIBHeader.dpiX, DIBHeader.dpiY);
+    printf("palette:\t%d\n", DIBHeader.palette);
+    printf("primary_color:\t%d\n", DIBHeader.primaryColorCount);
 
-    std::fseek(fp, fileHeader.offset, SEEK_SET);
+    fseek(fp, fileHeader.offset, SEEK_SET);
     int size = DIBHeader.width * DIBHeader.height;
 
     buffer = new char[DIBHeader.bits * size];
-    std::fread(buffer, 1, DIBHeader.bits * size, fp);
+    fread(buffer, 1, DIBHeader.bits * size, fp);
 
     isOpen = true;
 
-    std::fclose(fp);
+    fclose(fp);
     return true;
 }
 
@@ -76,5 +79,5 @@ Color XBitmap::getPixel(int x, int y) {
 }
 
 XBitmap::~XBitmap() {
-    delete buffer;
+    delete [] buffer;
 }
